38045a5dc1637e9b59fe17346050d0029f8d3555
Touch Media <upg.your.ads@gmial.com>
2017-09-19 14:56:00 +0500

    incommit

---
+++ Bridging_Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging_Header.h
+//  W-Music
+//
+//  Created by Touch Media on 16/06/2017.
+//  Copyright © 2017 ARTWEB. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "LGSideMenuController.h"
+#import "UIViewController+LGSideMenuController.h"
+
+
+#endif /* Bridging_Header_h */
---
+++ Urban_Taxi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Urban_Taxi
+//
+//  Created by Touch Media on 18/09/2017.
+//  Copyright © 2017 ARTWEB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /// Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
